83ef4afdb7109b6fb8ddb9a4deec82c82f3904d9
Dat Truong <d1nnn@192.168.1.19>
2023-11-24 10:47:16 +0700

    Them logic phan trang

--- client/src/components/blog/blog.component.js
+++ client/src/components/blog/blog.component.js
@@ -3,6 +3,8 @@ import BlogCard from "./blogcard.component";
 import "../../styles/blog/blog.style.css"
 import Pagination from "./pagination.component";
 
+const pagesShown = 5;
+
 const Blog = () => {
   const [{ blogs, totalCount }, setBlogs] = useState({
     blogs: undefined,
@@ -11,6 +13,11 @@ const Blog = () => {
   const [page, setPage] = useState(1);
   const [pages, setPages] = useState([]);
 
+  const arrayRange = (start, stop, step) =>
+    Array.from(
+      { length: (stop - start) / step + 1 },
+      (value, index) => start + index * step
+    );
 
 
   useEffect(() => {
@@ -22,7 +29,31 @@ const Blog = () => {
         blogs: data.blogs,
         totalCount: data.totalDocCount
       });
-      setPages([...Array(Math.floor(totalCount / 12) + 1 + (totalCount % 12 !== 0 ? 1 : 0)).keys()].slice(1))
+
+      //Tong so trang || Moi trang 12 documents || Co the fix 12 thanh so khac chia het cho 2 de show document deu` || Co the them query cho url la limit = 12 v.v
+      const totalPages = Math.floor(totalCount / 12) + (totalCount % 12 !== 0 ? 1 : 0);
+
+
+      // So trang nhieu hon so can show. Vi du 6 trang nhung can show 5 trang mot lan
+      if (totalPages > pagesShown) {
+        // Khoang giua cua pagesShown = 5 la 3, 3 cach trang cuoi 2 trang, cach trang dau 2 trang =>  Math.floor(5 / 2) == 2 == offset
+        let offset = Math.floor(pagesShown / 2);
+
+        // Trang hien tai (page) van co' offset hop le
+        if (page - offset > 0 && page + offset <= totalPages) {
+          setPages([page - 2, page - 1, page, page + 1, page + 2]);
+        } else {
+          // Trang hien tai nam o gan` cuoi'
+          if (page + offset >= totalPages)
+            setPages(arrayRange(totalPages - pagesShown + 1, totalPages, 1))
+          else if (page - offset < 0) // Trang hien tai nam` o~ dau`
+            setPages(arrayRange(1, pagesShown, 1))
+        }
+      }
+      else { //So trang it hon so trang minh can show. Vi du can show 5 trang mot lan`, nhung documents chi du~ cho 2 trang
+        setPages(arrayRange(1, totalPages, 1))
+
+      }
     }
 
     fetchBlogs();
