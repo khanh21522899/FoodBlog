007871802dcb1740d2d0dcc13792edc5daa69698
Dat Truong <d1nnn@192.168.1.16>
2023-11-30 18:20:48 +0700

    adding Links to Navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Link, NavLink } from 'react-router-dom';
+import "../styles/nav.style.css"
 
 const Navbar = () => {
   return (
-    <div>Nav Bar goes here</div>
+    <div className="navbar">
+      <NavLink to="/" style={({ isActive, isPending, isTransitioning }) => {
+        return {
+          fontWeight: isActive ? "bold" : "",
+          color: isPending ? "red" : "black",
+          viewTransitionName: isTransitioning ? "slide" : "",
+        };
+      }}>Home</NavLink>
+      <NavLink to="/create-blog" style={({ isActive, isPending, isTransitioning }) => {
+        return {
+          fontWeight: isActive ? "bold" : "",
+          color: isPending ? "red" : "black",
+          viewTransitionName: isTransitioning ? "slide" : "",
+        };
+      }}>Create Blog</NavLink>
+    </div>
   )
 }
 
