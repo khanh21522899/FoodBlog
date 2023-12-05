afcb707a8e74279b67b2687b9109ef5222a369fe
VoTuanNam <21522906@gm.uit.edu.vn>
2023-12-05 15:04:31 +0700

    update FE

--- client/src/pages/BlogDetail.js
+++ client/src/pages/BlogDetail.js
@@ -5,7 +5,7 @@ import Review from "../components/blog/review/review.component";
 import axios from "axios";
 import Rating from "../components/blog/review/Rating";
 import Message from "../components/blog/review/Message";
-import { Row, Col, ListGroup } from "react-bootstrap";
+import { ListGroup } from "react-bootstrap";
 
 export default function BlogDetail() {
   const { id } = useParams();
@@ -25,13 +25,6 @@ export default function BlogDetail() {
 
     // const fromData = new FormData(e.target);
     // console.log(fromData);
-  };
-  const submitHandler = (e) => {
-    e.preventDefault();
-    // createProductReview(match.params.id, {
-    //   rating,
-    //   comment,
-    // })
   };
 
   // Trang detail cua blog
@@ -70,67 +63,55 @@ export default function BlogDetail() {
         </div>
 
         <div className="Review">
-          <Form onSubmit={handleSubmit}>
-            <div className="form-group">
-              <label>Your review</label>
-              <textarea name="content" className="form-control" rows="3" />
-            </div>
-            <div className="form-group">
-              <button type="submit" className="btn btn-primary">
-                Submit
-              </button>
-            </div>
-            <div className="form-group"></div>
-
-            <h2>Reviews</h2>
-            {reviews.length === 0 && <Message>No Reviews</Message>}
-            <ListGroup variant="flush">
-              {reviews.map((review) => (
-                <ListGroup.Item key={review._id}>
-                  <strong>{review.name}</strong>
-                  <Rating value={review.rating} />
-                  <p>{review.comment}</p>
-                </ListGroup.Item>
-              ))}
-              <ListGroup.Item>
-                <form className="form" onSubmit={submitHandler}>
-                  <div>
-                    <h2>Write a customer review</h2>
-                  </div>
-                  <div>
-                    <label htmlFor="rating">Rating</label>
-                    <select
-                      id="rating"
-                      value={rating}
-                      onChange={(e) => setRating(e.target.value)}
-                    >
-                      <option value="">Select</option>
-                      <option value="1">1- Bad</option>
-                      <option value="2">2- Fair</option>
-                      <option value="3">3- Good</option>
-                      <option value="4">4- Very good</option>
-                      <option value="5">5- Excelent</option>
-                    </select>
-                  </div>
-                  <div>
-                    <label htmlFor="comment">Comment</label>
-                    <textarea
-                      id="comment"
-                      value={comment}
-                      onChange={(e) => setComment(e.target.value)}
-                    ></textarea>
-                  </div>
-
-                  <div>
-                    <label />
-                    <button className="primary" type="submit">
-                      Submit
-                    </button>
-                  </div>
-                </form>
+          <h2>Reviews</h2>
+          {reviews.length === 0 && <Message>No Reviews</Message>}
+          <ListGroup variant="flush">
+            {reviews.map((review) => (
+              <ListGroup.Item key={review._id}>
+                <strong>{review.name}</strong>
+                <Rating value={review.rating} />
+                <p>{review.comment}</p>
               </ListGroup.Item>
-            </ListGroup>
-          </Form>
+            ))}
+            <ListGroup.Item>
+              <form className="form" onSubmit={handleSubmit}>
+                <div>
+                  <h2>Write a customer review</h2>
+                </div>
+                <div>
+                  <label htmlFor="rating">Rating</label>
+                  <select
+                    id="rating"
+                    value={rating}
+                    onChange={(e) => setRating(e.target.value)}
+                  >
+                    <option value="">Select</option>
+                    <option value="1">1- Bad</option>
+                    <option value="2">2- Fair</option>
+                    <option value="3">3- Good</option>
+                    <option value="4">4- Very good</option>
+                    <option value="5">5- Excelent</option>
+                  </select>
+                </div>
+                <div>
+                  <label htmlFor="comment">Comment</label>
+                  <textarea
+                    id="comment"
+                    value={comment}
+                    onChange={(e) => setComment(e.target.value)}
+                  ></textarea>
+                </div>
+
+                <div>
+                  <label />
+                  <button className="primary" type="submit">
+                    Submit
+                  </button>
+                </div>
+              </form>
+            </ListGroup.Item>
+          </ListGroup>
+
           {reviews.map((review, index) => (
             <Review key={review} />
           ))}
