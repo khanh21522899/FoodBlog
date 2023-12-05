c79f446bb325c28ca96139a7d93782e4cc3f2df3
VoTuanNam <21522906@gm.uit.edu.vn>
2023-12-05 14:41:37 +0700

    update FE

--- client/src/pages/BlogDetail.js
+++ client/src/pages/BlogDetail.js
@@ -3,11 +3,16 @@ import { Form, useParams } from "react-router-dom";
 import "../styles/blog/blogdetail.style.css";
 import Review from "../components/blog/review/review.component";
 import axios from "axios";
+import Rating from "../components/blog/review/Rating";
+import Message from "../components/blog/review/Message";
+import { Row, Col, ListGroup } from "react-bootstrap";
 
 export default function BlogDetail() {
   const { id } = useParams();
   const [detail, setDetail] = useState(undefined);
   const [reviews, setReviews] = useState([1, 2, 3]);
+  const [rating, setRating] = useState(0);
+  const [comment, setComment] = useState("");
 
   const handleSubmit = (e) => {
     console.log(e.target.content.value);
@@ -20,6 +25,13 @@ export default function BlogDetail() {
 
     // const fromData = new FormData(e.target);
     // console.log(fromData);
+  };
+  const submitHandler = (e) => {
+    e.preventDefault();
+    // createProductReview(match.params.id, {
+    //   rating,
+    //   comment,
+    // })
   };
 
   // Trang detail cua blog
@@ -68,6 +80,56 @@ export default function BlogDetail() {
                 Submit
               </button>
             </div>
+            <div className="form-group"></div>
+
+            <h2>Reviews</h2>
+            {reviews.length === 0 && <Message>No Reviews</Message>}
+            <ListGroup variant="flush">
+              {reviews.map((review) => (
+                <ListGroup.Item key={review._id}>
+                  <strong>{review.name}</strong>
+                  <Rating value={review.rating} />
+                  <p>{review.comment}</p>
+                </ListGroup.Item>
+              ))}
+              <ListGroup.Item>
+                <form className="form" onSubmit={submitHandler}>
+                  <div>
+                    <h2>Write a customer review</h2>
+                  </div>
+                  <div>
+                    <label htmlFor="rating">Rating</label>
+                    <select
+                      id="rating"
+                      value={rating}
+                      onChange={(e) => setRating(e.target.value)}
+                    >
+                      <option value="">Select</option>
+                      <option value="1">1- Bad</option>
+                      <option value="2">2- Fair</option>
+                      <option value="3">3- Good</option>
+                      <option value="4">4- Very good</option>
+                      <option value="5">5- Excelent</option>
+                    </select>
+                  </div>
+                  <div>
+                    <label htmlFor="comment">Comment</label>
+                    <textarea
+                      id="comment"
+                      value={comment}
+                      onChange={(e) => setComment(e.target.value)}
+                    ></textarea>
+                  </div>
+
+                  <div>
+                    <label />
+                    <button className="primary" type="submit">
+                      Submit
+                    </button>
+                  </div>
+                </form>
+              </ListGroup.Item>
+            </ListGroup>
           </Form>
           {reviews.map((review, index) => (
             <Review key={review} />
