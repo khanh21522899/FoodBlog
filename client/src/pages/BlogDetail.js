9c06544c4b7ccb1cb6ab9d8492cd37b1a5ec7c38
VoTuanNam <21522906@gm.uit.edu.vn>
2023-12-06 20:04:59 +0700

    push comment

--- client/src/pages/BlogDetail.js
+++ client/src/pages/BlogDetail.js
@@ -3,9 +3,17 @@ import { Form, useParams } from "react-router-dom";
 import "../styles/blog/blogdetail.style.css";
 import Review from "../components/blog/review/review.component";
 import axios from "axios";
-import Rating from "../components/blog/review/Rating";
 import Message from "../components/blog/review/Message";
 import { ListGroup } from "react-bootstrap";
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBCol,
+  MDBContainer,
+  MDBIcon,
+  MDBRow,
+  MDBTypography,
+} from "mdb-react-ui-kit";
 
 export default function BlogDetail() {
   const { id } = useParams();
@@ -66,13 +74,111 @@ export default function BlogDetail() {
           <h2>Reviews</h2>
           {reviews.length === 0 && <Message>No Reviews</Message>}
           <ListGroup variant="flush">
-            {reviews.map((review) => (
+            {
+              /* {reviews.map((review) => (
               <ListGroup.Item key={review._id}>
                 <strong>{review.name}</strong>
                 <Rating value={review.rating} />
                 <p>{review.comment}</p>
               </ListGroup.Item>
-            ))}
+              
+            ))} */
+              <section className="vh-100">
+                <MDBContainer className="py-5" style={{ maxWidth: "1000px" }}>
+                  <MDBRow className="justify-content-center">
+                    <MDBCol md="11" lg="9" xl="7">
+                      <div className="d-flex flex-start mb-4">
+                        <img
+                          className="rounded-circle shadow-1-strong me-3"
+                          src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(32).webp"
+                          alt="avatar"
+                          width="65"
+                          height="65"
+                        />
+
+                        <MDBCard className="w-100">
+                          <MDBCardBody className="p-4">
+                            <div>
+                              <MDBTypography tag="h5">Johny Cash</MDBTypography>
+                              <p className="small">3 hours ago</p>
+                              <p>
+                                Cras sit amet nibh libero, in gravida nulla.
+                                Nulla vel metus scelerisque ante sollicitudin.
+                                Cras purus odio, vestibulum in vulputate at,
+                                tempus viverra turpis. Fusce condimentum nunc ac
+                                nisi vulputate fringilla. Donec lacinia congue
+                                felis in faucibus ras purus odio, vestibulum in
+                                vulputate at, tempus viverra turpis.
+                              </p>
+
+                              <div className="d-flex justify-content-between align-items-center">
+                                <div className="d-flex align-items-center">
+                                  <a href="#!" className="link-muted me-2">
+                                    <MDBIcon fas icon="thumbs-up me-1" />
+                                    132
+                                  </a>
+                                  <a href="#!" className="link-muted">
+                                    <MDBIcon fas icon="thumbs-down me-1" />
+                                    15
+                                  </a>
+                                </div>
+                                <a href="#!" className="link-muted">
+                                  <MDBIcon fas icon="reply me-1" /> Reply
+                                </a>
+                              </div>
+                            </div>
+                          </MDBCardBody>
+                        </MDBCard>
+                      </div>
+
+                      <div className="d-flex flex-start mb-4">
+                        <img
+                          className="rounded-circle shadow-1-strong me-3"
+                          src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(31).webp"
+                          alt="avatar"
+                          width="65"
+                          height="65"
+                        />
+
+                        <MDBCard className="w-100">
+                          <MDBCardBody className="p-4">
+                            <div>
+                              <MDBTypography tag="h5">
+                                Mindy Campbell
+                              </MDBTypography>
+                              <p className="small">5 hours ago</p>
+                              <p>
+                                Lorem ipsum dolor sit, amet consectetur
+                                adipisicing elit. Delectus cumque doloribus
+                                dolorum dolor repellat nemo animi at iure autem
+                                fuga cupiditate architecto ut quam provident
+                                neque, inventore nisi eos quas?
+                              </p>
+
+                              <div className="d-flex justify-content-between align-items-center">
+                                <div className="d-flex align-items-center">
+                                  <a href="#!" className="link-muted me-2">
+                                    <MDBIcon fas icon="thumbs-up me-1" />
+                                    158
+                                  </a>
+                                  <a href="#!" className="link-muted">
+                                    <MDBIcon fas icon="thumbs-down me-1" />
+                                    13
+                                  </a>
+                                </div>
+                                <a href="#!" className="link-muted">
+                                  <MDBIcon fas icon="reply me-1" /> Reply
+                                </a>
+                              </div>
+                            </div>
+                          </MDBCardBody>
+                        </MDBCard>
+                      </div>
+                    </MDBCol>
+                  </MDBRow>
+                </MDBContainer>
+              </section>
+            }
             <ListGroup.Item>
               <form className="form" onSubmit={handleSubmit}>
                 <div>
