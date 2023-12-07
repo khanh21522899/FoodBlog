8746e6978325496a786126b40ab0bc0bff6a760e
khanh21522899n <21522899@gm.uit.edu.vn>
2023-12-07 22:55:24 +0700

    some-reaction-acording-to-user-status

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,20 @@
 import React from 'react'
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink , useNavigate} from 'react-router-dom';
 import "../styles/nav.style.css"
+import { useAuthContext } from '../hooks/useAuthContext';
+import {useLogout} from '../hooks/useLogout'
 
-const Navbar = ({ user }) => {
-  console.log(user);
+const Navbar = () => {
+  const {user} = useAuthContext()
+  const navigate = useNavigate()
+  const logout = useLogout()
+
+  const handleLogout = async e => {
+    e.preventDefault();
+    await logout()
+    navigate('/')
+  }
+
   return (
     <div className="navbar">
       <NavLink to="/" style={({ isActive, isPending, isTransitioning }) => {
@@ -13,8 +24,9 @@ const Navbar = ({ user }) => {
           viewTransitionName: isTransitioning ? "slide" : "",
         };
       }}>Home</NavLink>
-      <NavLink to="/blogs/create-blog" style={({ isActive, isPending, isTransitioning }) => {
+      <NavLink to={"/blogs/create-blog"} style={({ isActive, isPending, isTransitioning }) => {
         return {
+          display: user? "" : "none",
           fontWeight: isActive ? "bold" : "",
           color: isPending ? "red" : "black",
           viewTransitionName: isTransitioning ? "slide" : "",
@@ -26,7 +38,8 @@ const Navbar = ({ user }) => {
           color: isPending ? "red" : "black",
           viewTransitionName: isTransitioning ? "slide" : "",
         };
-      }}>{user ? user.name + "user" : "Login"}</NavLink>
+      }}>{user ? user.email : "Login"}</NavLink>
+      <button onClick={handleLogout} style={user ?{}:{display :"none"}}>Log out</button>
     </div>
   )
 }
