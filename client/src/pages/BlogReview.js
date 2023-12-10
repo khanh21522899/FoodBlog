bbc4dc29024e82785c488b7aaa3e29b6c20f5df6
VoTuanNam <21522906@gm.uit.edu.vn>
2023-12-11 01:05:37 +0700

    fix bug va dj ngu

--- client/src/pages/BlogReview.js
+++ client/src/pages/BlogReview.js
@@ -1,19 +1,9 @@
 import React, { useEffect, useState } from "react";
 import Review from "../components/blog/review/review.component";
-import Message from "../components/blog/review/Message";
 import { ListGroup } from "react-bootstrap";
 import { Form, useParams } from "react-router-dom";
 import axios from "axios";
-
-import {
-  MDBCard,
-  MDBCardBody,
-  MDBCol,
-  MDBContainer,
-  MDBIcon,
-  MDBRow,
-  MDBTypography,
-} from "mdb-react-ui-kit";
+import "./BlogReview.css";
 import { useAuthContext } from "../hooks/useAuthContext";
 
 // const review = () => {
@@ -30,7 +20,6 @@ export default function BlogReviews() {
   const [reviews, setReviews] = useState([]);
   const [rating, setRating] = useState(0);
   const [comment, setComment] = useState("");
-  const [data, setData] = useState(null);
   const [pages, setPages] = useState(1);
   const { user } = useAuthContext();
   // console.log(user);
@@ -54,15 +43,16 @@ export default function BlogReviews() {
     e.preventDefault();
     // console.log(e.target.comment.value);
     const dataAxios = await axios.post(`/api/v1/reviews/`, {
-      content: e.target.comment.value,
-      rating: e.target.rating.value,
+      content: comment,
+      rating: rating,
       blogId: blogId,
-      userId: user.id,
+      userId: user?.id,
       date: new Date(),
     });
     console.log(dataAxios);
     fetchReview();
-
+    setComment("");
+    setRating(0);
     // const fromData = new FormData(e.target);
     // console.log(fromData);
   };
@@ -80,49 +70,53 @@ export default function BlogReviews() {
         <div className="Review">
           <h2>Reviews</h2>
           {/* {reviews.length === 0 && <Message>No Reviews</Message>} */}
-          <ListGroup variant="flush">
-            <ListGroup.Item>
-              <form className="form" onSubmit={handleSubmit}>
-                <div>
-                  <h2>Write a customer review</h2>
-                </div>
-                <div>
-                  <label htmlFor="rating">Rating</label>
-                  <select
-                    id="rating"
-                    value={rating}
-                    onChange={(e) => setRating(e.target.value)}
-                  >
-                    <option value="">Select</option>
-                    <option value="1">1- Bad</option>
-                    <option value="2">2- Fair</option>
-                    <option value="3">3- Good</option>
-                    <option value="4">4- Very good</option>
-                    <option value="5">5- Excelent</option>
-                  </select>
-                </div>
-                <div>
-                  <label htmlFor="comment">Comment</label>
-                  <textarea
-                    id="comment"
-                    value={comment}
-                    onChange={(e) => setComment(e.target.value)}
-                  ></textarea>
-                </div>
+          {user ? (
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <form className="form" onSubmit={handleSubmit}>
+                  <div>
+                    <h2>Write a customer review</h2>
+                  </div>
+                  <div>
+                    <label htmlFor="rating">Rating</label>
+                    <select
+                      id="rating"
+                      value={rating}
+                      onChange={(e) => setRating(e.target.value)}
+                    >
+                      <option value="">Select</option>
+                      <option value="1">1- Bad</option>
+                      <option value="2">2- Fair</option>
+                      <option value="3">3- Good</option>
+                      <option value="4">4- Very good</option>
+                      <option value="5">5- Excelent</option>
+                    </select>
+                  </div>
+                  <div>
+                    <label htmlFor="comment">Comment</label>
+                    <textarea
+                      id="comment"
+                      value={comment}
+                      onChange={(e) => setComment(e.target.value)}
+                    ></textarea>
+                  </div>
 
-                <div>
-                  <label />
-                  <button
-                    className="primary"
-                    type="submit"
-                    // onClick={fetchReview}
-                  >
-                    Submit
-                  </button>
-                </div>
-              </form>
-            </ListGroup.Item>
-          </ListGroup>
+                  <div>
+                    <label />
+                    <button
+                      className="primary"
+                      type="submit"
+                      // onClick={fetchReview}
+                    >
+                      Submit
+                    </button>
+                  </div>
+                </form>
+              </ListGroup.Item>
+            </ListGroup>
+          ) : (
+            <h2>Đăng nhập để review</h2>
+          )}
 
           {reviews.map((review, index) => (
             <Review
